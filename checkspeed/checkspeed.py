e6bef4c507eac93233765bbe428ca655de7b0ea2
slasjh <127065284+slasjh@users.noreply.github.com>
2025-03-06 04:25:27 +0000

    Update checkspeed.py

--- checkspeed/checkspeed.py
+++ checkspeed/checkspeed.py
@@ -17,7 +17,7 @@ BlackHost=["127.0.0.1:8080","live3.lalifeier.eu.org","newcntv.qcloudcdn.com","ot
 
 # 读取文件内容
 def read_txt_file(file_path):
-    skip_strings = ['#genre#']  # 定义需要跳过的字符串数组['#', '@', '#genre#'] 
+    skip_strings = ['#genre#']  # 定义需要跳过的字符串 数组['#', '@', '#genre#'] 
     required_strings = ['://']  # 定义需要包含的字符串数组['必需字符1', '必需字符2'] 
 
     with open(file_path, 'r', encoding='utf-8') as file:
