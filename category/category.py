cfe8760e38ed0e2cc4cc7e33783f9bc5fdd4ffb5
slasjh <127065284+slasjh@users.noreply.github.com>
2025-03-13 04:23:42 +0000

    Update category.py

--- category/category.py
+++ category/category.py
@@ -134,6 +134,38 @@ def tiqu_gjz(output_file, feilei, gjz_or_gjzs):
     except Exception as e:
         print(f"保存文件时发生错误：{e}")
 
+def tiqu_gjz_all(output_file, feilei, gjz_or_gjzs):
+    try:
+        # 假设all_lines是从某个地方获取的文本行列表
+        # 这里为了示例，我们将其硬编码在函数内部
+        #all_lines = [
+            #"这是一行测试文本。",
+            #"包含chinamobile.com的文本行：http://www.chinamobile.com/something",
+            #"另一行包含migu的文本：http://example.com/migu.php",
+            #"还有一行包含mg的文本：http://example.com/mg.php",
+            #"以及一行不包含目标网址的文本。"
+        #]
+
+        # 如果gjz_or_gjzs是字符串，则将其转换为单元素集合以便统一处理
+        if isinstance(gjz_or_gjzs, str):
+            gjz_set = {gjz_or_gjzs}
+        else:
+            gjz_set = set(gjz_or_gjzs)
+
+        with open(output_file, 'w', encoding='utf-8') as f:
+            # 注意：这里我们不再写入gjz_or_gjzs到文件，因为它可能是多个值
+            # 如果您确实需要写入某种标识符，请考虑使用feilei参数
+            f.write(f'{feilei},#genre#\n')  # 使用f-string格式化字符串并写入分类信息
+            for line in all_lines:
+                if all(gjz in line for gjz in gjz_set):
+                    f.write(line + '\n')
+
+        print(f"合并后的文本已保存到文件: {output_file}")
+        #print("time: {}".format(datetime.datetime.now().strftime("%Y%m%d_%H_%M_%S")))
+
+    except Exception as e:
+        print(f"保存文件时发生错误：{e}")
+        
 all_lines =  []
 #读取文本
 excudelist_lines=read_txt_to_array('category/ExcludeList.txt') 
@@ -210,6 +242,14 @@ output_file14 = "category/ziwei.txt"
 feilei14 = "自慰分类"
 gjz14 = [ "自慰"]  # 使用列表来存储多个关键字
 
+output_file88 = "category/3plulianjiao.txt"
+feilei188 = "3p露脸叫分类"
+gjz188 = [ "3p", "脸", "叫"]  # 使用列表来存储多个关键字
+output_file89 = "category/houlianru.txt"
+feilei189 = "后入脸叫分类"
+gjz189 = [ "后入", "脸", "乳"]  # 使用列表来存储多个关键字
+
+
 # 调用函数示例，注意现在第三个参数对于第二个文件是一个列表
 tiqu_gjz(output_file1, feilei1, gjz1)
 tiqu_gjz(output_file2, feilei2, gjz2)
@@ -226,4 +266,6 @@ tiqu_gjz(output_file12, feilei12, gjz12)
 tiqu_gjz(output_file13, feilei13, gjz13)
 tiqu_gjz(output_file14, feilei14, gjz14)
 
- 
+# 调用函数示例，注意现在第三个参数对于第二个文件是一个列表
+tiqu_gjz_all(output_file88, feilei88, gjz88) 
+tiqu_gjz_all(output_file89, feilei89, gjz89) 
