8fa2305f11919b2b314db2121a3d911e14dfdc58
slasjh <127065284+slasjh@users.noreply.github.com>
2026-02-26 15:11:05 +0800

    Update checkspeed.py

--- checkspeed/checkspeed.py
+++ checkspeed/checkspeed.py
@@ -358,15 +358,15 @@ if __name__ == "__main__":
     parent_dir = os.path.dirname(current_dir)
     
     input_file1 = os.path.join(current_dir, 'live.txt')  # 输入文件路径1
-    input_file2 = os.path.join(current_dir, 'blacklist_auto.txt')  # 输入文件路径2
+    #input_file2 = os.path.join(current_dir, 'blacklist_auto.txt')  # 输入文件路径2
     success_file = os.path.join(current_dir, 'whitelist_auto.txt')  # 成功清单文件路径
     success_file_tv = os.path.join(current_dir, 'whitelist_auto_tv.txt')  # 成功清单文件路径（另存一份直接引用源）
     blacklist_file = os.path.join(current_dir, 'blacklist_auto.txt')  # 黑名单文件路径
 
     # 读取输入文件内容
     lines1 = read_txt_file(input_file1)
-    lines2 = read_txt_file(input_file2)
-    lines=urls_all_lines + lines1 + lines2 # 从list变成集合提供检索效率⇒发现用了set后加#合并多行url，故去掉
+    #lines2 = read_txt_file(input_file2)
+    lines=urls_all_lines + lines1  # 从list变成集合提供检索效率⇒发现用了set后加#合并多行url，故去掉
     #lines=urls_all_lines  # Test
     
     # 计算合并后合计个数
